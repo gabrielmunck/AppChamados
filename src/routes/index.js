214aa02946cd7c34acfba2291fc1ea528943dd30
Gabriel A. Munck P. da Cruz <munckgabriel@gmail.com>
2023-04-27 01:11:45 -0300

    Rota principal protegida para apenas pessoas logadas e apos deslogar apagar dados do localstorage

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,13 +4,15 @@ import SignIn from '../pages/SignIn'
 import SignUp from '../pages/SignUp/signUp'
 import Dashboard from '../pages/Dashboard';
 
+import Private from './Private.js';
+
 function RoutesApp() {
     return (
 
         <Routes>
-            <Route path='/' element={ <SignIn /> } />
-            <Route path='/register' element={ <SignUp /> } />
-            <Route path='/dashboard' element={ <Dashboard /> } />
+            <Route path='/' element={<SignIn />} />
+            <Route path='/register' element={<SignUp />} />
+            <Route path='/dashboard' element={<Private> <Dashboard /> </Private>} />
         </Routes>
 
     )
