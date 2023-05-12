3d43bdeff6220377db7ac5598f7a6ab6b490d294
Gabriel A. Munck P. da Cruz <munckgabriel@gmail.com>
2023-05-12 19:58:15 -0300

    Criada logica de imagem do perfil e uma parte do estilo

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,8 +1,19 @@
 import Header from '../../components/Header'
 import Title from '../../components/Title'
-import { FiSettings } from "react-icons/fi";
+import { FiSettings, FiUpload } from "react-icons/fi";
+import avatar from "../../assets/avatar.png"
+
+import { useContext, useState } from 'react';
+import { AuthContext } from '../../contexts/auth';
+
+import './profile.css'
 
 export default function Profile() {
+
+    const { user } = useContext(AuthContext) // Pegando dados do User atraves do contexto
+
+    const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl) // Se tiver um user puxara a foto de perfil do usuario, se nao, ira ficar como estado nulo
+
     return (
         <div>
             <Header />
@@ -10,6 +21,38 @@ export default function Profile() {
                 <Title name='Minha Conta'>
                     <FiSettings size={26} />
                 </Title>
+
+                <div className='container'>
+
+                    <form className='form-profile'>
+
+                        <label className='label-avatar'>
+
+                            <span> <FiUpload color='#FFF' size={25} /> </span>
+                            <input type='file' accept='image/*' /> <br />
+                            {avatarUrl === null ? (          // Estando em estado nulo puxara a imagem padrao avatar
+                                <img src={avatar} alt='foto de perfil' width={250} height={250} />
+                            ) : (
+                                <img src={avatarUrl} alt='foto de perfil' width={250} height={250} /> // Se nao, puxa a imagem que o usuario inseriu
+                            )}
+
+                        </label>
+
+                        <label>Nome</label>
+                        <input type='text' placeholder='Digite seu nome' />
+
+                        <label>Email</label>
+                        <input type='text' placeholder='teste@teste.com' disabled={true} />
+
+                        <button type='submit'>Salvar</button>
+
+                    </form>
+                </div>
+                
+                <div className='container'>
+                    <button className='logout'>Sair</button>
+                </div>
+
             </div>
         </div>
     )
