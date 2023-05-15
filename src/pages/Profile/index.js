1a74a490b2ae700e5bb38f5de1241f4d8f495785
Gabriel A. Munck P. da Cruz <munckgabriel@gmail.com>
2023-05-15 18:24:19 -0300

    Terminada pagina de perfil com funcao que envia a nome e imagem para o storage, obtem a URL da imagem e atualiza o banco com os novos dados

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -6,6 +6,12 @@ import avatar from "../../assets/avatar.png"
 import { useContext, useState } from 'react';
 import { AuthContext } from '../../contexts/auth';
 
+import { db, storage } from '../../services/firebaseConnection';
+import { doc, updateDoc } from 'firebase/firestore'
+import { uploadBytes, ref, getDownloadURL } from 'firebase/storage';
+
+import { toast } from 'react-toastify';
+
 import './profile.css'
 
 export default function Profile() {
@@ -18,62 +24,128 @@ export default function Profile() {
     const [email, setEmail] = useState(user && user.email)
 
     function handleFile(e) {
-        if(e.target.files[0]) {
+        if (e.target.files[0]) {
             const image = e.target.files[0]
 
-            if(image.type === 'image/jpeg' || image.type === 'image/png') {
+            if (image.type === 'image/jpeg' || image.type === 'image/png') {
                 setImageAvatar(image)
                 setAvatarUrl(URL.createObjectURL(image)) // Criando uma URL para a imagem, caso ela seja do tipo indicado acima 
             } else {
-                alert ("Envie uma imagem com o formato PNG ou JPEG.")
+                alert("Envie uma imagem com o formato PNG ou JPEG.")
                 setImageAvatar(null)
                 return
             }
         }
     }
 
+    async function handleUpload(e) { // Envia a imagem para o storage, obtem a URL e atualiza o banco com os novos dados (podendo usar em qualquer lugar atraves do contexto, devido ao objeto criado la)
+
+        const currentUid = user.uid
+
+        const uploadRef = ref(storage, `image/${currentUid}/${imageAvatar.name}`)
+
+        const uploadTask = uploadBytes(uploadRef, imageAvatar)
+            .then((snapshot) => {
+
+                getDownloadURL(snapshot.ref).then(async (downloadURL) => {
+
+                    let urlFoto = downloadURL;
+
+                    const docRef = doc(db, "users", user.uid)
+
+                    await updateDoc(docRef, {
+                        nome: nome,
+                        avatarUrl: urlFoto,
+                    })
+                        .then(() => {
+                            let data = {
+                                ...user,
+                                nome: nome,
+                                avatarUrl: urlFoto,
+                            }
+
+                            setUser(data)
+                            storageUser(data)
+                            toast.success("Perfil Atualizado!")
+
+                        })
+
+                    })
+
+                })
+
+            }
+
+    async function handleSubmit(e) { // Funcao para atualizar as informacoes no db
+                    e.preventDefault();
+
+                    if (imageAvatar === null && nome !== '') { // Atualiza apenas o nome, pois nao identifica nenhuma imagem 
+
+                        const docRef = doc(db, "users", user.uid)
+                        await updateDoc(docRef, {
+                            nome: nome,
+                        })
+
+                            .then(() => {
+                                let data = {
+                                    ...user,
+                                    nome: nome
+                                }
+
+                                setUser(data)
+                                storageUser(data)
+                                toast.success("Perfil Atualizado!")
+
+                            })
+
+                    } else if (imageAvatar !== null && nome !== '') { //Atualiza nome e imagem
+
+                        handleUpload()
+
+                    }
+                }
 
     return (
-        <div>
-            <Header />
-            <div className='content'>
-                <Title name='Minha Conta'>
-                    <FiSettings size={26} />
-                </Title>
+            <div>
+                <Header />
+                <div className='content'>
+                    <Title name='Minha Conta'>
+                        <FiSettings size={26} />
+                    </Title>
 
-                <div className='container'>
+                    <div className='container'>
 
-                    <form className='form-profile'>
+                        <form className='form-profile' onSubmit={handleSubmit}>
 
-                        <label className='label-avatar'>
+                            <label className='label-avatar'>
 
-                            <span> <FiUpload color='#FFF' size={25} /> </span>
-                            <input type='file' accept='image/*' onChange={handleFile}/> <br />
-                            {avatarUrl === null ? (          // Estando em estado nulo puxara a imagem padrao avatar
-                                <img src={avatar} alt='foto de perfil' width={250} height={250} />
-                            ) : (
-                                <img src={avatarUrl} alt='foto de perfil' width={250} height={250} /> // Se nao, puxa a imagem que o usuario inseriu
-                            )}
+                                <span> <FiUpload color='#FFF' size={25} /> </span>
+                                <input type='file' accept='image/*' onChange={handleFile} /> <br />
+                                {avatarUrl === null ? (          // Estando em estado nulo puxara a imagem padrao avatar
+                                    <img src={avatar} alt='foto de perfil' width={250} height={250} />
+                                ) : (
+                                    <img src={avatarUrl} alt='foto de perfil' width={250} height={250} /> // Se nao, puxa a imagem que o usuario inseriu
+                                )}
 
-                        </label>
+                            </label>
 
-                        <label>Nome</label>
-                        <input type='text' value={nome} onChange={(e) => setNome(e.target.value)} />
+                            <label>Nome</label>
+                            <input type='text' value={nome} onChange={(e) => setNome(e.target.value)} />
 
-                        <label>Email</label>
-                        <input type='text' value={email} disabled={true} />
+                            <label>Email</label>
+                            <input type='text' value={email} disabled={true} />
 
-                        <button type='submit'>Salvar</button>
+                            <button type='submit'>Salvar</button>
 
-                    </form>
-                </div>
+                        </form>
+                    </div>
 
-                <div className='container'>
-                    <button className='logout-btn' onClick={ () => logout() }>Sair</button>
+                    <div className='container'>
+                        <button className='logout-btn' onClick={() => logout()}>Sair</button>
+                    </div>
+
                 </div>
 
             </div>
-
-        </div>
-    )
-}
+        )
+    }
