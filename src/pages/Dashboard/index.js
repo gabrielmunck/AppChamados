8ab7386aaa369e3f955b6d78d3c4aad3588b19c5
Gabriel A. Munck P. da Cruz <munckgabriel@gmail.com>
2023-05-20 04:23:39 -0300

    Primeira parte da pagina principal feita, adicionando estrutura do site e começo de uma tabela dinamica

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,14 @@
 import { AuthContext } from "../../contexts/auth"
 import { useContext } from "react"
 
-import  Header  from "../../components/Header";
+import { Link } from "react-router-dom";
 
+import Header from "../../components/Header";
+import Title from '../../components/Title'
+
+import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from "react-icons/fi";
+
+import './dashboard.css'
 
 
 export default function Dashboard() {
@@ -15,11 +21,50 @@ export default function Dashboard() {
 
     return (
         <div>
+            <Header />     
 
-            <Header/>
-            <h1>Pagina Dashboard</h1>
-            <br/>
-            <button onClick={handleLogout}>Sair da conta</button>
+             <div className="content">
+
+                <Title name='Tickets'> <FiMessageSquare size={25}/> </Title>
+
+             
+             
+                <>
+
+                    <Link to='/new' className="new"> 
+                        <FiPlus color="#FFF" size={25}/> 
+                        Novo Chamado 
+                    </Link>
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <th scope="col">Cliente</th>
+                                <th scope="col">Assunto</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Cadastrado em</th>
+                                <th scope="col">#</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td data-label='Cliente'> Mercado Esquina </td>
+                                <td data-label='Assunto'> Suporte </td>
+                                <td data-label='Status'> Em Aberto </td>
+                                <td data-label='Cadastrado'> 12/05/2022 </td>
+                                <td data-label='#'>
+                                    <button className="action" style={ {backgroundColor: '#3583f6'} }> <FiSearch color="#FFF" size={17}/> </button>
+                                    <button className="action" style={ {backgroundColor: '#f6a935'} }> <FiEdit2 color="#FFF" size={17}/> </button>
+                                    
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                </>
+
+             </div>
+
         </div>
     )
 }
