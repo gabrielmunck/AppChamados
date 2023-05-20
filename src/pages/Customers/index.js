1d62044f14ce14963758426f9132f2012dfe7253
Gabriel A. Munck P. da Cruz <munckgabriel@gmail.com>
2023-05-20 01:46:24 -0300

    Criado caminho para cadastro de clientes no db e verificação para erro

--- src/pages/Customers/index.js
+++ src/pages/Customers/index.js
@@ -4,6 +4,11 @@ import Header from "../../components/Header"
 import Title from "../../components/Title"
 
 import { FiUser } from "react-icons/fi";
+import { toast } from "react-toastify";
+
+import { db } from '../../services/firebaseConnection'
+import { addDoc, collection } from "firebase/firestore";
+
 
 export default function Customers() {
 
@@ -11,12 +16,42 @@ export default function Customers() {
     const [cnpj, setCnpj] = useState('')
     const [endereco, setEndereco] = useState('')
 
-    function handleRegister(e){
+    async function handleRegister(e) {
+
         e.preventDefault()
 
-        alert()
+        if (nome !== '' && cnpj !== '' && endereco !== '') {
+
+            await addDoc(collection(db, 'customers'), {
+
+                nomeFantasia: nome,
+                cnpj: cnpj,
+                endereco: endereco
+
+            })
+
+            .then(() => {
+
+                setNome('')
+                setCnpj('')
+                setEndereco('')
+                toast.success('Empresa Registrada')
+            })
+
+            .catch((error) => {
+                console.log(error)
+                toast.error("Erro ao fazer cadastro")
+            })
+
+            
+        } else {
+
+            toast.error("Preencha todas as informações !")
+            
+        }
+
     }
-    
+
     return (
 
         <div>
@@ -27,7 +62,7 @@ export default function Customers() {
 
                 <Title name='Clientes'>
 
-                    <FiUser size={25}/>
+                    <FiUser size={25} />
 
                 </Title>
 
@@ -37,16 +72,16 @@ export default function Customers() {
                     <form className="form-profile" onSubmit={handleRegister}>
 
                         <label>Nome Fantasia</label>
-                        <input type="text" placeholder="Nome da Empresa" 
-                        value={nome} onChange={(e) => setNome(e.target.value)}/>
+                        <input type="text" placeholder="Nome da Empresa"
+                            value={nome} onChange={(e) => setNome(e.target.value)} />
 
                         <label>CNPJ</label>
-                        <input type="text" placeholder="CNPJ da Empresa" 
-                        value={cnpj} onChange={(e) => setCnpj(e.target.value)}/>
+                        <input type="text" placeholder="CNPJ da Empresa"
+                            value={cnpj} onChange={(e) => setCnpj(e.target.value)} />
 
                         <label>Endereco</label>
-                        <input type="text" placeholder="Endereco da Empresa" 
-                        value={endereco} onChange={(e) => setEndereco(e.target.value)}/>
+                        <input type="text" placeholder="Endereco da Empresa"
+                            value={endereco} onChange={(e) => setEndereco(e.target.value)} />
 
                         <button type="submit">
                             Cadastrar
